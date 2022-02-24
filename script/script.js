dcd6cfbe65fcf42fb095de7cd9ad5e4fac3c4a53
Tiago de Lima Batista <tigolimab223@gmail.com>
2022-02-24 11:30:36 -0300

    fix

--- script/script.js
+++ script/script.js
@@ -1,5 +1,27 @@
-function inicio(){
-    let nomeUsuario = prompt("Seja Bem-vindo a Fashion Driven, insira o seu nome aqui: ")
+/* Variaveis Globais */
+
+let nomeUsuario = prompt("Bem vindo à DrivenFashion \nQual por qual nome podemos te chamar?")
+
+/* Funções */
+function selecionar() {
+    let selecionarProduto = document.querySelector(".produto")
+    selecionarProduto.addEventListener('click', evento)
+    console.log("uepa")
 }
 
-inicio()
+function evento(evento) {
+    console.log(evento.target.classList)
+    if (evento.target.classList === '.imagem-produto') {
+        console.log("oi")
+        evento.target.classList.toggle('selecionado');
+    }
+    else {
+        return false
+    }
+};
+
+function pegarDados(){
+    let dados = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts")
+    console.log(dados)
+}
+pegarDados()
